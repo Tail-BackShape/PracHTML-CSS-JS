256d77e2a1c612cfb31ebdeaed60f8d91b724463
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-03-08 23:21:18 +0900

    add index.html and script.js to radioAlert
    this can alert the selected radio button

---
+++ radioAlert/script.js
@@ -0,0 +1,20 @@
+function showRadioState() {
+  // ラジオボタンの要素を取得
+  var radioButtons = document.getElementsByName("radioGroup");
+
+  // 選択されているラジオボタンを検索
+  var selectedOption;
+  for (var i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      selectedOption = radioButtons[i].value;
+      break;
+    }
+  }
+
+  // 選択されているラジオボタンがあればアラートで表示
+  if (selectedOption) {
+    alert("選択されているオプション: " + selectedOption);
+  } else {
+    alert("ラジオボタンが選択されていません。");
+  }
+}
