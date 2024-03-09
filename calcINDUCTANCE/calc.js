ec700b8785816355ba4ddcfe7ca2af60db307092
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-03-09 23:29:19 +0900

    fix calc.js and index.html
    Unit calculations can now be performed via radio buttons.

--- calcINDUCTANCE/calc.js
+++ calcINDUCTANCE/calc.js
@@ -4,65 +4,95 @@ function calcInductance(freq, capa) {
     console.log(na);
 }
 
-function altHunit() {
-    // ラジオボタンの要素を取得
-    var radioButtons = document.getElementsByName("L");
+function getElementsUnit() {
+    var LUnit = getLunit();
+    var fUnit = getfunit();
+    var CUnit = getCunit();
 
-    // 選択されているラジオボタンを検索
-    var selectedOption;
-    for (var i = 0; i < radioButtons.length; i++) {
-    if (radioButtons[i].checked) {
-        selectedOption = radioButtons[i].value;
-        break;
-        }
-    }
-
-    // 選択されているラジオボタンがあればアラートで表示
-    if (selectedOption) {
-        alert("選択されているオプション: " + selectedOption);
-    } else {
-        alert("ラジオボタンが選択されていません。");
-    }
+    alert("L: " + String(LUnit) + "f: " + String(fUnit) + "C: " + String(CUnit));
 }
 
-function altfunit() {
+function getLunit() {
     // ラジオボタンの要素を取得
-    var radioButtons = document.getElementsByName("f");
+    var Lunit = document.getElementsByName("L");
 
     // 選択されているラジオボタンを検索
     var selectedOption;
-    for (var i = 0; i < radioButtons.length; i++) {
-    if (radioButtons[i].checked) {
-        selectedOption = radioButtons[i].value;
+    for (var i = 0; i < Lunit.length; i++) {
+    if (Lunit[i].checked) {
+        selectedOption = Lunit[i].value;
         break;
         }
     }
 
-    // 選択されているラジオボタンがあればアラートで表示
-    if (selectedOption) {
-        alert("選択されているオプション: " + selectedOption);
-    } else {
-        alert("ラジオボタンが選択されていません。");
+    switch (selectedOption) {
+        case "μH":
+            selectedOption = 1e-6;
+            break;
+        case "mH":
+            selectedOption = 1e-3;
+            break;
+        case "H":
+            selectedOption = 1;
+            break;
     }
+
+    return selectedOption;
 }
 
-function altCunit() {
+function getfunit() {
     // ラジオボタンの要素を取得
-    var radioButtons = document.getElementsByName("C");
+    var funit = document.getElementsByName("f");
 
     // 選択されているラジオボタンを検索
     var selectedOption;
-    for (var i = 0; i < radioButtons.length; i++) {
-    if (radioButtons[i].checked) {
-        selectedOption = radioButtons[i].value;
+    for (var i = 0; i < funit.length; i++) {
+    if (funit[i].checked) {
+        selectedOption = funit[i].value;
         break;
         }
     }
 
-    // 選択されているラジオボタンがあればアラートで表示
-    if (selectedOption) {
-        alert("選択されているオプション: " + selectedOption);
-    } else {
-        alert("ラジオボタンが選択されていません。");
+    switch (selectedOption) {
+        case "Hz":
+            selectedOption = 1;
+            break;
+        case "kHz":
+            selectedOption = 1e3;
+            break;
+        case "MHz":
+            selectedOption = 1e6;
+            break;
     }
+
+    return selectedOption;
+}
+
+
+function getCunit() {
+    // ラジオボタンの要素を取得
+    var Cunit = document.getElementsByName("C");
+
+    // 選択されているラジオボタンを検索
+    var selectedOption;
+    for (var i = 0; i < Cunit.length; i++) {
+    if (Cunit[i].checked) {
+        selectedOption = Cunit[i].value;
+        break;
+        }
+    }
+
+    switch (selectedOption) {
+        case "pF":
+            selectedOption = 1e-12;
+            break;
+        case "nF":
+            selectedOption = 1e-9;
+            break;
+        case "μF":
+            selectedOption = 1e-6;
+            break;
+    }
+
+    return selectedOption;
 }
